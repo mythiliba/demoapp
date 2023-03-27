883b060e55193554512704c6aef6da2493273a0a
mythiliba <mythilibala1985@gmail.com>
2023-03-27 22:24:33 +0530

    Update node.js

--- Terraform/node.js
+++ Terraform/node.js
@@ -2,6 +2,6 @@ const express = require('express')
 const app = express()
 const port = 80
 
-app.get('/', (req, res) => res.send('! Terraform demoapp !'))
+app.get('/', (req, res) => res.send('! Terraform demoapp deploy !'))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
